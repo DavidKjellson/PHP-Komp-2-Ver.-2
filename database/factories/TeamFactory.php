91fb79aae7041f6388c98687a5c6dc34558eae59
David Kjellson <david.kjellson@gmail.com>
2021-05-11 00:59:49 +0200

    Team factory and such

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Team;
+use Faker\Generator as Faker;
+
+$factory->define(Team::class, function (Faker $faker) {
+    return [
+        'team_name' => $faker->name,
+        'team_activity' => ''
+    ];
+});
+
+// namespace Database\Factories;
+
+// use App\Models\Team;
+// use Illuminate\Database\Eloquent\Factories\Factory;
+
+// class TeamFactory extends Factory
+// {
+//     /**
+//      * The name of the factory's corresponding model.
+//      *
+//      * @var string
+//      */
+//     protected $model = Team::class;
+
+//     /**
+//      * Define the model's default state.
+//      *
+//      * @return array
+//      */
+//     public function definition()
+//     {
+//         return [
+//             //
+//         ];
+//     }
+// }
